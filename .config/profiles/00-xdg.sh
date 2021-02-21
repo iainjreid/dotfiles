14963bf297da027da806a85ac95ef7b71ac7ebab
Iain Reid <iainreid382@gmail.com>
2021-02-21 17:59:37 +0000

    Split apart .profile configurations

---
+++ .config/profiles/00-xdg.sh
@@ -0,0 +1,26 @@
+${XDG_CONFIG_HOME:="$HOME/.config"}
+${XDG_CACHE_HOME:="$HOME/.cache"}
+
+${XDG_DATA_HOME:="$HOME/.local/share"}
+${XDG_BIN_HOME:="$HOME/.local/bin"}
+${XDG_LIB_HOME:="$HOME/.local/lib"}
+
+${XDG_DATA_DIRS:="/usr/local/share:/usr/share"}
+${XDG_BIN_DIRS:="/usr/local/bin:/usr/bin"}
+${XDG_LIB_DIRS:="/usr/local/lib:/usr/lib"}
+
+${XDG_RUNTIME_DIR:="/tmp"}
+
+export XDG_CONFIG_HOME
+export XDG_CACHE_HOME
+
+export XDG_DATA_HOME
+export XDG_BIN_HOME
+export XDG_LIB_HOME
+
+export XDG_DATA_DIRS
+export XDG_BIN_DIRS
+export XDG_LIB_DIRS
+
+export XDG_RUNTIME_DIR
+
---
+++ .config/profiles/50-rust.sh
@@ -0,0 +1,6 @@
+# Cargo
+export CARGO_HOME="$XDG_DATA_HOME/cargo"
+
+# Expose local Rust binaries
+export PATH="$CARGO_HOME/bin:$PATH"
+
